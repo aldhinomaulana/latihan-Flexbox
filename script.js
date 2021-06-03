8c0173c2c56c9f48594ade7bd3918d4ea6783b67
aldhinomaulana <aldhinomaulana06gmail.com>
2021-06-03 14:40:51 +0700

    menambahkan index.html,script.s dan style.css

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggel input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click',function() {
+  nav,classlist,toggle('slide');
+});
